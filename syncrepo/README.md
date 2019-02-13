2377d673e696550494ae1f2d3364455f5b3b3b7e
Sugesh Chandran <sugeshchandran@gmail.com>
2019-02-13 14:21:42 +0000

    Update the syncrepo scripts to use more file types
    Signed-off-by: Sugesh Chandran <sugeshchandran@gmail.com>

--- syncrepo/README.md
+++ syncrepo/README.md
@@ -9,11 +9,11 @@ server for testing. The reason for this approach is
 Copying the changes to the remote repo is bit difficult. The script uses rsync
 to do it. It will copy the diff to the server for the relevant testing.
 
+Currently it copies C, C++, h files. Update the script to add more file types
+if needed.
+
 ## How to use it.
 
 1. Update the credentials in the script with corresponding details.
 
-2. Copy the script to code top directory in the vm.
-
-3. Run the script each time when a change is being made.
-
+2. Run the script each time when a change is being made.
--- syncrepo/sync_repo.sh
+++ syncrepo/sync_repo.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -x
-REMOTE_REPO=/home/sugeshch/repo/FPGA-BDX-POC
-REMOTE_USER=root
-REMOTE_IP=10.01.0.1
+LOCAL_REPO=/home/sugchand/local-vm/repo/MyMatrix/
+REMOTE_REPO=/home/sugchand/remote-server/repo/MyMatrix/
+REMOTE_USER=sugchand
+REMOTE_IP=1.2.3.4
 
-rsync -av --include '*/' --include '*.mk' --include '*.sh' --include '*.c' --include '*.h' --include '*.in' --exclude '*' . REMOTE_USER@REMOTE_IP:$REMOTE_REPO
-#rsync -av --include '*/' --include '*.mk' --include '*.sh' --include '*.c' --include '*.h' --include '*.in' --exclude '*' . root@10.237.223.42:$REMOTE_REPO
+rsync -avrhu --progress --include '*/' --include '*.mk' --include '*.sh' --include '*.c' --include '*.h' --include '*.in' --include '*.cc' --include '*.txt' --include '*.md' --exclude '*' $LOCAL_REPO $REMOTE_USER@$REMOTE_IP:$REMOTE_REPO
+#rsync -av --include '*/' --include '*.mk' --include '*.sh' --include '*.c' --include '*.h' --include '*.in' --include '*.cc' --exclude '*' . root@10.237.223.42:$REMOTE_REPO
 
